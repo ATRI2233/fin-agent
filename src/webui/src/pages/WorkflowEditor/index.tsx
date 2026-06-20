928c538512c94de73daeac288e60e80fcde834a5
ATRII <1695758364@qq.com>
2026-06-20 12:28:21 +0800

    fix: TS类型 WorkflowEdge + pytest sys.path 添加 pythonpath

--- src/webui/src/pages/WorkflowEditor/index.tsx
+++ src/webui/src/pages/WorkflowEditor/index.tsx
@@ -189,8 +189,8 @@ function WorkflowEditorInner() {
     workflowId: id ?? null,
     getSaveData: () => ({
       name: workflowName,
-      nodes: nodes as Node[],
-      edges: edges as Edge[],
+      nodes: nodes as WorkflowNode[],
+      edges: edges as WorkflowEdge[],
     }),
   });
 
